eeeb90f47fe2e42931cedf87d6016a743c4adc62
Costinel22 <malaiasi_costinel@yahoo.com>
2024-02-03 18:32:01 +0000

    add desing on html page and connect api

--- assets/js/details_page.js
+++ assets/js/details_page.js
@@ -4,9 +4,40 @@ function showDetails() {
     h2.insertAdjacentText("beforeend", text);
 }
 
-var apiKey = "1";
+$(document).ready(function() {
+            function displayFoodInfo() {
+                var queryURL = "https://api.edamam.com/api/recipes/v2/68b1dd6da9193b21372bee7a2bd19941?type=public&app_id=7bfc5058&app_key=72f32efdcea683bd1f0a63347aefc9a7";
 
-fetch(apiKey)
-    .then(function(response) {
-        return response
+                fetch(queryURL)
+                    .then(response => response.json())
+                    .then(data => {
+                            var imageUrl = data.recipe.image;
+                            var label = data.recipe.label;
+                            var cuisineType = data.recipe.cuisineType[0];
+                            var ingredients = data.recipe.ingredientLines;
+                            var dietLabels = data.recipe.dietLabels;
+                            var mealType = data.recipe.mealType;
+                            var dishType = data.recipe.dishType;
+                            var calories = data.recipe.calories;
+                            jQuery("#insertHere").html(`
+        <h3>Cuisine Type: ${cuisineType}</h3>
+        <h4>Meal Type: ${mealType.map(mealType => `${mealType}`).join("")}</4>
+        <h4>Dish Type: ${dishType.map(dishType => `${dishType}`).join("")}</4>
+        <h4>Calories: ${calories}</4>
+        <p>Ingredients:</p>
+        <ul>
+        ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join("")}
+        </ul>
+        <p>Diet Labels:</p>
+        <ul>
+        ${dietLabels.map(dietLabels => `<li>${dietLabels}</li>`).join("")}
+        </ul>
+        `);
+        jQuery("#image-suport").html(`
+        <img src="${imageUrl}" alt="Recipe Image">
+        <h2>${label}</h2>
+        `);
     });
+}  
+displayFoodInfo();
+});
