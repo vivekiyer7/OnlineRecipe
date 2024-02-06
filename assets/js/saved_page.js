3e3bb2c6b3ae1907db5b1d4cf6bf25723253c989
Joy Onugha <joyonugha@gmail.com>
2024-02-06 13:46:58 +0000

    2nd commit for Saved Recipe branch

--- assets/js/saved_page.js
+++ assets/js/saved_page.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
-    $('.saveBtn').on('click', function () {
-        var name = $(this).siblings('.description').val();
-        var image = $(this).siblings().attr('id');
-        var description = $(this).parent().attr('id');
-    })
-});
+    const API_Key = "58c03c765fc3561060028e9cde96da90";
+    const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+    const FORECAST_BASE_URL = "https://api.openweathermap.org/data/2.5/forecast";
+    
+
+
+//     $('.saveBtn').on('click', function () {
+//         var name = $(this).siblings('.description').val();
+//         var image = $(this).siblings().attr('id');
+//         var description = $(this).parent().attr('id');
+//     })
+// });
 
